7ab75ddfb57f62a84dc44b63be4ac5a97af58fea
Alexander Ortiz <vtacius@gmail.com>
2014-09-05 08:12:30 -0600

    Primera idea del día

--- ipeador.py
+++ ipeador.py
@@ -3,26 +3,28 @@ import re
 
 cidr = sys.argv[1]
 
-direccion = re.search("((\d{1,3})\.*(\d{1,3})\.*(\d{1,3})\.*(\.*\d{1,3}))\/(\d{1,2})",cidr);
+direccion = re.search("((\d{1,3})\.*(\d{1,3})\.*(\d{1,3})\.*(\.*\d{1,3}))\/(\d{1,2})", cidr);
 ip_addr = direccion.group(1)
 sufijo = direccion.group(6)
 bite = (int(sufijo) // 8 ) + 2
 bites = int(sufijo) % 8 
-mask = "1"*bites
+mask = "1" * bites
 byte = bin(int(direccion.group(bite)))[2:]
+while len(byte) !=8:
+	byte = "0" + byte
 
 l = 0
 caso = True if (len(mask)>0) else False
 octal = []
 while caso:
-    print("byte sobre el que se va a evaluar: ", mask[l], byte[l])
     if (mask[l]==byte[l]):
         l += 1
         octal.append(byte[l])
         caso = True if (len(mask)>1) else False
     else:
-        print ("Esto hace la diferencia", mask[l], byte[l], int(mask[1]) & int(byte[l]))
         caso = False
 
 # Probar con 192.168.2.193/27
+print(mask)
+print(byte)
 print(octal)
